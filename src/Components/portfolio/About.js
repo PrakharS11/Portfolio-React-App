9b9d751c93dc200d78a285063d61c7060639b686
Prakhar Srivastava <prakharsahai21@gmail.com>
2022-09-29 02:01:31 +0530

    sixth commit

--- src/Components/portfolio/About.js
+++ src/Components/portfolio/About.js
@@ -9,6 +9,7 @@ function About() {
             <section className="portfolio-about-content-wrapper">
                 <h2>All about this portfolio website</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing</p>
+                <a target="_blank" href="https://inspiring-roentgen-5fa24e.netlify.app/" >Visit Website</a>
             </section>
         )
     }
